4293ea40397c2d3b0164613b42333515ebe75ffd
Mogamat-S23 <saleemariefdien@gmail.com>
2022-06-20 14:15:35 +0200

    new css

--- script/code.js
+++ script/code.js
@@ -9,6 +9,21 @@ c.addEventListener('click', () => {
     
     // let O = document.getElementById('output').value;
     answer = eval((m * 60));
-        document.getElementById('output').innerHTML = answer ;
-        document.getElementById('output1').innerHTML = 'seconds' ;
+        document.getElementById('output').innerHTML = answer + 'seconds' ;
+        // document.getElementById('output1').innerHTML = 'seconds' ;
+        output.value = answer + ' seconds'
+        
 })
+
+
+let btnClear = document.getElementById('clear');
+
+btnClear.addEventListener('click', () => {
+    let age = document.getElementById('minutes')
+    minutes.value = ''
+    // document.getElementById('output1').innerHTML = ''
+    document.getElementById('output').innerHTML = ''
+    output.value = ''
+
+
+});
